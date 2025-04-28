f3def98974bb14292fd3fd9db8cd8190e3fa61bc
Rachel H Barcellona <rbarcellona@tampabay.rr.com>
2025-04-28 16:49:32 -0400

    Add files via upload

---
+++ locations:models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Location(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.state}"
---
+++ locations:urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import LocationViewSet
+
+router = DefaultRouter()
+router.register(r'locations', LocationViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('locations.urls')),
+]
